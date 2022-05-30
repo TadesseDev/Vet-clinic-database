d04a948268bc4f30be71eefb62674ffa7222c6e6
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-30 22:37:15 +0300

    Create queries

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
-
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals
+VALUES (DEFAULT, 'Agumon', '2020-2-3', 0, TRUE, 10.23),
+  (DEFAULT, 'Gabumon', '2018-11-15', 2, TRUE, 8),
+  (DEFAULT, 'Pikachu', '2021-1-7', 1, FALSE, 15.04),
+  (DEFAULT, 'Devimon', '2017-5-12', 5, TRUE, 11);
--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 /* Database schema to keep the structure of entire database. */
-drop table animals;
 CREATE TABLE if not exists animals (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100),
