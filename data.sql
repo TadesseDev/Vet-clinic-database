f4611bb0356091863b6753a7954c71d4caad817b
Tadesse-Alemayehu <email24t@gmail.com>
2022-05-30 23:11:32 +0300

    Update insertion

--- data.sql
+++ data.sql
@@ -1,6 +1,12 @@
 /* Populate database with sample data. */
-INSERT INTO animals
-VALUES (DEFAULT, 'Agumon', '2020-2-3', 0, TRUE, 10.23),
-  (DEFAULT, 'Gabumon', '2018-11-15', 2, TRUE, 8),
-  (DEFAULT, 'Pikachu', '2021-1-7', 1, FALSE, 15.04),
-  (DEFAULT, 'Devimon', '2017-5-12', 5, TRUE, 11);
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES ('Agumon', '2020/2/3', 0, TRUE, 10.23),
+  ('Gabumon', '2018/11/15', 2, TRUE, 8),
+  ('Pikachu', '2021/1/7', 1, FALSE, 15.04),
+  ('Devimon', '2017/5/12', 5, TRUE, 11);
