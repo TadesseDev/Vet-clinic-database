50e6d38c51a03a9ccba2c1197a654ca3d9aa4022
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-05-31 11:08:21 +0300

    Clear previouse project CREATE schema

--- schema.sql
+++ schema.sql
@@ -1,11 +1,2 @@
-/* Database schema to keep the structure of entire database. */
-CREATE TABLE if not exists animals (
-    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(100),
-    date_of_birth DATE NOT NULL,
-    escape_attempts FLOAT NOT NULL,
-    neutered BOOLEAN NOT NULL,
-    weight_kg DECIMAL NOT NULL
-);
 ALTER TABLE animals
 ADD COLUMN species varchar(250);
